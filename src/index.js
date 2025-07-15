25e2477f24e051fc1202e611c4236e80991c8d86
HarrisonKanellos <harrisonkanellos@gmail.com>
2025-07-15 13:59:25 +1000

    Add logic to handle module rendering on nav click

--- src/index.js
+++ src/index.js
@@ -4,3 +4,32 @@ import { renderMenuContent } from "./content-modules/menu";
 import { renderContactContent } from "./content-modules/contact-us";
 
 renderHomeContent();
+
+const nav = document.querySelector("nav");
+nav.addEventListener(("click"), handleNavClick);
+
+function handleNavClick(e) {
+    const id = e.target.id;
+
+    switch (id) {
+        case "home":
+            clearContent();
+            renderHomeContent();
+            break;
+        case "menu":
+            clearContent();
+            renderMenuContent();
+            break;
+        case "contact-us":
+            clearContent();
+            renderContactContent();
+    }
+}
+
+function clearContent() {
+    const contentContainer = document.querySelector("#content");
+    const child = contentContainer.querySelector(":first-child");
+    if (child !== null) {
+        contentContainer.removeChild(child);
+    }
+}
